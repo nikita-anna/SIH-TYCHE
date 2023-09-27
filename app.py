c68427e7ba7a64945240be715e1223e2d8f2894b
namnair <93312156+namnair@users.noreply.github.com>
2023-09-27 17:02:48 +0530

    added appropriate content

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ def handle_userinput(user_question):
 
 def main():
     load_dotenv()
-    st.set_page_config(page_title="Chat with multiple PDFs",
+    st.set_page_config(page_title="Legal Buddy",
                        page_icon=":books:")
     st.write(css, unsafe_allow_html=True)
 
@@ -75,8 +75,8 @@ def main():
     if "chat_history" not in st.session_state:
         st.session_state.chat_history = None
 
-    st.header("Chat with multiple PDFs :books:")
-    user_question = st.text_input("Ask a question about your documents:")
+    st.header("Legal Buddy :books:")
+    user_question = st.text_input("Ask me a question about your documents:")
     if user_question:
         handle_userinput(user_question)
 
